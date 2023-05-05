b7db13d36e6ec36262df25c3117073f40acface4
John Hunt <john.hunt10@gmail.com>
2023-05-05 18:58:40 +0100

    formnatting

--- chapter3_slots/main.py
+++ chapter3_slots/main.py
@@ -7,7 +7,7 @@ class Play:
         self.year = year
 
     def __repr__(self):
-        return f'Play({self.title} by {self.author} in {self.year})'
+        return f'Play("{self.title}", by {self.author} in {self.year})'
 
 
 play = Play('Alls Well That Ends Well',
