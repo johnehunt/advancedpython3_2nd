64e4a935a65d8cd8f19b66a8f5a2688ee54c4f0e
John Hunt <john.hunt10@gmail.com>
2023-05-09 13:23:34 +0100

    Initial import

---
+++ chapter03_slots/person.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def birthday(self):
+        print(f'Happy birthday {self.name}, you were {self.age}', end = '')
+        self.age = self.age + 1
+        print(f' but now you are {self.age}')
+
+p1 = Person('Phoebe', 25)
+print(f'p1: {p1.name} {p1.age}')
+
+p1.address = '10 High Street'
+print(f'p1.address: {p1.address}')
+
+p2 = Person('Gryff', 24)
+print(f'p2: {p2.name} {p2.age}')
+print(f'p2.address: {p2.address}')
