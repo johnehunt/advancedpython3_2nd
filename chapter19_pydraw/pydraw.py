ad24fd84741b4677ef7e8373ed0b167c94240b07
John Hunt <john@matraining.com>
2023-06-15 15:12:22 +0100

    Added next level of functionality

--- chapter19_pydraw/pydraw.py
+++ chapter19_pydraw/pydraw.py
@@ -5,27 +5,59 @@ HEIGHT = 400
 
 BACKGROUND_COLOUR = 'white'
 
+CIRCLE = 'Circle'
+SQUARE = 'Square'
+LINE = 'Line'
+
+SIZE = 20
+
 
 class PyDraw:
-    def __init__(self, root):
-        self.root = root
+    def __init__(self):
+        self.root = tk.Tk()
         self.canvas = tk.Canvas(self.root, width=WIDTH, height=HEIGHT, bg=BACKGROUND_COLOUR)
         self.canvas.pack()
-        self.canvas.bind('<B1-Motion>', self.draw)
-        self.create_menu()
+        self.canvas.bind('<Button-1>', self.draw)
+        self.mode = SQUARE
+        # Set up menus
+        self.menubar = tk.Menu(self.root)
+        self.root.config(menu=self.menubar)
+        self.create_file_menu()
+        self.create_mode_menu()
 
-    def create_menu(self):
-        menubar = tk.Menu(self.root)
-        self.root.config(menu=menubar)
+    def create_mode_menu(self):
+        mode_menu = tk.Menu(self.menubar, tearoff=0)
+        mode_menu.add_command(label=CIRCLE, command=self.set_circle_mode)
+        mode_menu.add_command(label=SQUARE, command=self.set_square_mode)
+        mode_menu.add_command(label=LINE, command=self.set_line_mode)
+        self.menubar.add_cascade(label='Mode', menu=mode_menu)
 
-        file_menu = tk.Menu(menubar, tearoff=0)
+    def create_file_menu(self):
+        file_menu = tk.Menu(self.menubar, tearoff=0)
         file_menu.add_command(label='New', command=self.new_canvas)
         file_menu.add_command(label='Exit', command=self.exit_app)
-        menubar.add_cascade(label='File', menu=file_menu)
+        self.menubar.add_cascade(label='File', menu=file_menu)
+
+    def set_circle_mode(self):
+        print(f'Set circle mode')
+        self.mode = CIRCLE
+
+    def set_square_mode(self):
+        print(f'Set square mode')
+        self.mode = SQUARE
+
+    def set_line_mode(self):
+        print(f'Draw line mode')
+        self.mode = LINE
 
     def draw(self, event):
         x, y = event.x, event.y
-        self.canvas.create_oval(x - 2, y - 2, x + 2, y + 2, fill='black')
+        if self.mode == CIRCLE:
+            self.canvas.create_oval(x, y, x + SIZE, y + SIZE, fill='red')
+        elif self.mode == SQUARE:
+            self.canvas.create_rectangle(x, y, x + SIZE, y + SIZE, fill= 'blue')
+        elif self.mode == LINE:
+            self.canvas.create_line(x, y, x + SIZE, y + SIZE)
 
     def new_canvas(self):
         self.canvas.delete('all')
@@ -35,9 +67,8 @@ class PyDraw:
 
 
 def main():
-    root = tk.Tk()
-    app = PyDraw(root)
-    root.mainloop()
+    app = PyDraw()
+    app.root.mainloop()
 
 
 if __name__ == '__main__':
